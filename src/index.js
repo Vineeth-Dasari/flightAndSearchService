b43aa81c994cd00c6846f52201db95fa1cde77ad
VINEETH DASARI <dasariwineethoo7@gmail.com>
2024-04-27 13:12:44 +0530

    basic bady parser setup is implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 //require('dotenv').config()  // this requires the dotenv variable we have setup
 const { PORT } = require('./config/serverConfig');
+const bodyparser = require("body-parser"); //Middleware
 
 
 const setupAndStartServer = async () => {
 
     const app = express();
     
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended : true}));
 
     app.listen(PORT, () => {
         console.log(`Server started at port = ${PORT}`);

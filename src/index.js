e7740c3676cc3b1304f1b2bc22b1279aac6e3a42
VINEETH DASARI <dasariwineethoo7@gmail.com>
2024-04-27 13:08:15 +0530

    .env is deleted

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 const express = require("express");
 //require('dotenv').config()  // this requires the dotenv variable we have setup
-
 const { PORT } = require('./config/serverConfig');
 
 
 const setupAndStartServer = async () => {
 
     const app = express();
-    app.listen(PORT, () => {
+    
 
+    app.listen(PORT, () => {
         console.log(`Server started at port = ${PORT}`);
         //console.log(process.env); // Just for tracking our varibales in proces.env object.
-
-
     })
 
 
